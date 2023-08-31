da1ebb06721eb72513360cae2e1d95ae5b250bff
Adan Gonzalez Flores <108160165+Apanawa@users.noreply.github.com>
2023-08-30 18:45:49 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyAcDav9Z9vutKMqFNNiJ8h2HCZTKucN6Q2IrwhavjyaOPNeAUd-FT_6uclCtcMjNDYyg/exec";
+const AS_URL_BASE = "https://script.google.com/macros/library/d/1hsfgcTK4dRitrhRstoUEHmVGPW2D1McjPj_FhgR7tvRoCs6Qr49ckICt/1";
