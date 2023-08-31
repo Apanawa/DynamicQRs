e94732c828cc22d0d9499e57f4962f3ddd0c788d
Adan Gonzalez Flores <108160165+Apanawa@users.noreply.github.com>
2023-08-30 18:25:54 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbyAcDav9Z9vutKMqFNNiJ8h2HCZTKucN6Q2IrwhavjyaOPNeAUd-FT_6uclCtcMjNDYyg/exec";
